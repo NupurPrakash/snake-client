544b16e4a4df64d577604b3433abb5df3f61da45
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-23 20:21:51 +0000

    used connection object for setupInput

--- play.js
+++ play.js
@@ -1,33 +1,10 @@
-//const net = require('net');
-//const key;
-/*
-const handleUserInput = function(key) {
-  if(key === '\u0003') {
-    process.exit();
-  }
-}
-*/
+
 const connect = require('./client');
 const setupInput = require('./input');
-//const handleUserInput = require('./client');
 console.log('Connecting...');
 connect();
-/*
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data',handleUserInput)
 
-
-  
-  return stdin;
-}
-*/
-setupInput();
-
-
+setupInput(connect);
 
 
 
