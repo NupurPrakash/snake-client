c79ec8bc61c420798c6bad585c36cc68fa174543
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-23 18:04:36 +0000

    handled user input

--- play.js
+++ play.js
@@ -1,4 +1,32 @@
+//const net = require('net');
+//const key;
 
+const handleUserInput = function(key) {
+  if(key === '\u0003') {
+    process.exit();
+  }
+}
 const connect = require('./client');
+//const handleUserInput = require('./client');
 console.log('Connecting...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data',handleUserInput)
+
+
+  
+  return stdin;
+}
+setupInput();
+
+
+
+
+
+
+
