ced4637d521d189b0e07245c138eeb64d5373157
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-23 20:19:31 +0000

    implemented wasd movements

--- input.js
+++ input.js
@@ -1,17 +1,47 @@
+let connection;
+const net = require('net');
 const handleUserInput = function(key) {
   if(key === '\u0003') {
     process.exit();
   }
+  if(key === 'w') {
+    connection.write("Move: up");
+  }
+  if(key === 'a') {
+    connection.write("Move: left");
+  }
+  if(key === 's') {
+    connection.write("Move: down");
+  }
+  if(key === 'd') {
+    connection.write("Move: right");
+  }
+  
 }
 
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
+  connection = net.createConnection({
+    host : 'localhost',
+    port : 50541
+  });
+  connection.setEncoding('utf8');
+  
+ 
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data',handleUserInput);
   return stdin;
+
+ 
+
+
+
+
 }
 
 module.exports = setupInput;
+
